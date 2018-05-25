81e746543f943beaddae929e31f7028305f3b9ee
Pedro Henrique Moura Pereira <phenrique.0206@gmail.com>
2018-05-25 11:19:38 -0300

    Add files via upload

---
+++ obrigado.php
@@ -0,0 +1,33 @@
+<center>
+<!-- <img src='http://192.168.100.75/index_arquivos/image001.jpg' border='0' /> -->
+
+<center> <a href="http://psi.educacao"><img src='logo-educacao.jpg'></a> </center>
+
+    <form method="post" action="verif.php" name="form" AUTOCOMPLETE='ON' onSubmit="return valida()">
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+
+<font size="20" > OBRIGADO!</font> <br />
+
+<center/>
+
